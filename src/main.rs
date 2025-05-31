5f0ee7142538053c235a575b4e47c0487801c300
Ryan Butler <thebutlah@gmail.com>
2025-05-31 17:47:27 -0400

    replace code with functioning bevy sprite animation

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,100 @@
-// Support configuring Bevy lints within code.
-#![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
-// Disable console on Windows for non-dev builds.
-#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
+use std::time::Duration;
 
-mod asset_tracking;
-mod audio;
-mod demo;
-#[cfg(feature = "dev")]
-mod dev_tools;
-mod menus;
-mod screens;
-mod theme;
+use bevy::{
+	DefaultPlugins,
+	app::{App, Startup, Update},
+	asset::{AssetServer, Assets},
+	core_pipeline::core_2d::Camera2d,
+	ecs::{
+		component::Component,
+		system::{Commands, Query, Res, ResMut},
+	},
+	image::{TextureAtlas, TextureAtlasLayout},
+	math::UVec2,
+	prelude::{Deref, DerefMut, PluginGroup},
+	render::texture::ImagePlugin,
+	sprite::Sprite,
+	time::{Time, Timer, TimerMode},
+};
 
-use bevy::{asset::AssetMetaCheck, prelude::*};
-
-fn main() -> AppExit {
-	App::new().add_plugins(AppPlugin).run()
+fn main() {
+	App::new()
+		.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+		.add_systems(Startup, setup)
+		.add_systems(Update, animate_sprite)
+		.run();
 }
 
-pub struct AppPlugin;
+fn setup(
+	mut commands: Commands,
+	asset_server: Res<AssetServer>,
+	mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+	commands.spawn(Camera2d);
 
-impl Plugin for AppPlugin {
-	fn build(&self, app: &mut App) {
-		// Add Bevy plugins.
-		app.add_plugins(
-			DefaultPlugins
-				.set(AssetPlugin {
-					// Wasm builds will check for meta files (that don't exist) if this isn't set.
-					// This causes errors and even panics on web build on itch.
-					// See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-					meta_check: AssetMetaCheck::Never,
-					..default()
-				})
-				.set(WindowPlugin {
-					primary_window: Window {
-						title: "Jam6".to_string(),
-						fit_canvas_to_parent: true,
-						..default()
-					}
-					.into(),
-					..default()
-				}),
-		);
+	let width = 6;
+	let layout = TextureAtlasLayout::from_grid(
+		// UVec2 { x: 23, y: 27 },
+		UVec2 { x: 33, y: 33 },
+		width,
+		2,
+		None,
+		None,
+	);
+	let texture_atlas_layout = texture_atlas_layouts.add(layout);
+	let run_animation_idx = AnimationIndices::new(width, 1, 0..width);
+	let animation_timer =
+		AnimationTimer(Timer::new(Duration::from_millis(100), TimerMode::Repeating));
+	commands.spawn((
+		Sprite::from_atlas_image(
+			asset_server.load("images/ducky.png"),
+			TextureAtlas {
+				layout: texture_atlas_layout,
+				index: run_animation_idx.first,
+			},
+		),
+		run_animation_idx,
+		animation_timer,
+	));
+}
 
-		#[cfg(feature = "dev")]
-		compile_error!("yeet");
+#[derive(Component)]
+struct AnimationIndices {
+	first: usize,
+	last: usize,
+}
 
-		// Add other plugins.
-		app.add_plugins((
-			asset_tracking::plugin,
-			audio::plugin,
-			demo::plugin,
-			#[cfg(feature = "dev")]
-			dev_tools::plugin,
-			menus::plugin,
-			screens::plugin,
-			theme::plugin,
-		));
+impl AnimationIndices {
+	fn new(width: u32, row: u32, indices: impl Into<std::ops::Range<u32>>) -> Self {
+		let indices: std::ops::Range<u32> = indices.into();
+		let first = row * width + indices.start;
+		let last = first + indices.end - 1;
 
-		// Order new `AppSystems` variants by adding them here:
-		app.configure_sets(
-			Update,
-			(
-				AppSystems::TickTimers,
-				AppSystems::RecordInput,
-				AppSystems::Update,
-			)
-				.chain(),
-		);
-
-		// Set up the `Pause` state.
-		app.init_state::<Pause>();
-		app.configure_sets(Update, PausableSystems.run_if(in_state(Pause(false))));
-
-		// Spawn the main camera.
-		app.add_systems(Startup, spawn_camera);
+		Self {
+			first: first as usize,
+			last: last as usize,
+		}
 	}
 }
 
-/// High-level groupings of systems for the app in the `Update` schedule.
-/// When adding a new variant, make sure to order it in the `configure_sets`
-/// call above.
-#[derive(SystemSet, Debug, Clone, Copy, Eq, PartialEq, Hash, PartialOrd, Ord)]
-enum AppSystems {
-	/// Tick timers.
-	TickTimers,
-	/// Record player input.
-	RecordInput,
-	/// Do everything else (consider splitting this into further variants).
-	Update,
-}
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
 
-/// Whether or not the game is paused.
-#[derive(States, Copy, Clone, Eq, PartialEq, Hash, Debug, Default)]
-#[states(scoped_entities)]
-struct Pause(pub bool);
+fn animate_sprite(
+	time: Res<Time>,
+	mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut Sprite)>,
+) {
+	for (indices, mut timer, mut sprite) in &mut query {
+		timer.tick(time.delta());
 
-/// A system set for systems that shouldn't run while the game is paused.
-#[derive(SystemSet, Copy, Clone, Eq, PartialEq, Hash, Debug)]
-struct PausableSystems;
-
-fn spawn_camera(mut commands: Commands) {
-	commands.spawn((Name::new("Camera"), Camera2d));
+		if timer.just_finished() {
+			if let Some(atlas) = &mut sprite.texture_atlas {
+				atlas.index = if atlas.index == indices.last {
+					indices.first
+				} else {
+					atlas.index + 1
+				};
+			}
+		}
+	}
 }
