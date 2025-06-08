34008b2b885492678173642aa0fd8d77ace020f0
Malek <pocmalek@gmail.com>
2025-06-08 14:46:00 -0400

    awa

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 fn main() {
-    dotenv::dotenv().ok();
+   /* dotenv::dotenv().ok();
     println!("cargo:rustc-env=LEADERBOARD_ONE={}", env::var("LEADERBOARD_ONE").unwrap());
-    println!("cargo:rustc-env=LEADERBOARD_TWO={}", env::var("LEADERBOARD_TWO").unwrap());
+    println!("cargo:rustc-env=LEADERBOARD_TWO={}", env::var("LEADERBOARD_TWO").unwrap());*/
 }
